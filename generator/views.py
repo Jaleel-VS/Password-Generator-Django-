b1774a416c8a0c1af070df39fac3f94718b52f7f
Jaleel-VS <jeremikress@gmail.com>
2021-11-25 21:00:22 +0200

    add home view

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,15 @@
+from random import choice
+
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    # names = ('Joseph', 'Mary', 'Steve', 'Peter')
+    # return HttpResponse(f'Hello, there {choice(names)} ')
+    return render(request, 'generator/home.html', {'password': '11rqfadqi'})
+
+
+def eggs(request):
+    return HttpResponse('I like eggs')
