66587db472b756a2ebfdec259dfbc870429882ef
Jaleel-VS <jeremikress@gmail.com>
2021-11-26 21:30:46 +0200

    add password and about views to urls

--- generator/views.py
+++ generator/views.py
@@ -1,4 +1,8 @@
 from random import choice
+from string import (ascii_lowercase as lower,
+                    ascii_uppercase as upper,
+                    punctuation as special,
+                    digits as numbers)
 
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -8,8 +12,31 @@ from django.http import HttpResponse
 def home(request):
     # names = ('Joseph', 'Mary', 'Steve', 'Peter')
     # return HttpResponse(f'Hello, there {choice(names)} ')
-    return render(request, 'generator/home.html', {'password': '11rqfadqi'})
+    return render(request, 'generator/home.html')
 
 
-def eggs(request):
-    return HttpResponse('I like eggs')
+def password(request):
+    all_characters = lower
+
+    password_options = {
+        'uppercase': upper,
+        'special': special,
+        'numbers': numbers,
+    }  # 2
+
+    for k, v in password_options.items():
+        if request.GET.get(k):
+            all_characters += v
+
+    length = int(request.GET.get('length', 12))
+
+    generated_password = ''
+
+    for x in range(length):
+        generated_password += choice(all_characters)
+
+    return render(request, 'generator/password.html', {'password': generated_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -19,6 +19,7 @@ from generator import views
 
 urlpatterns = [
     # path('hello/', admin.site.urls),
-    path('', views.home),
-    path('eggs/', views.eggs),
+    path('', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('about/', views.about, name='about'),
 ]
